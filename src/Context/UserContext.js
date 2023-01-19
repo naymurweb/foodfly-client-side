a188a6f6731cd713b6eb9e9822f0640b7e3b4e0c
naymurweb <naymurweb@gmail.com>
2023-01-19 21:38:03 +0600

    user register account

--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,7 +1,48 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
 export const AuthContext = createContext();
+const auth = getAuth(app);
+
 const UserContext = ({ children }) => {
-  const info = { name: "Mr pranto" };
+  const [user, setUser] = useState({});
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+//   const updateUser = (userName) => {
+//     return updateProfile(auth.currentUser, {
+//       displayName: userName,
+//     });
+//   };
+
+  const userProfileUpdate = (userName, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: userName,
+      photoURL: photo,
+    });
+  };
+
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        console.log("user sign out!");
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  console.log("hhh", user);
+
+  const info = { createUser, userProfileUpdate  };
   return (
     <div>
       <AuthContext.Provider value={info}>{children}</AuthContext.Provider>
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,10 +1,32 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Context/UserContext";
 
 const Register = () => {
-  const data=useContext(AuthContext)
-  console.log(data);
+  const { createUser,userProfileUpdate  } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const createUserHandaler = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photo=form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        form.reset();
+        userProfileUpdate (name,photo)
+        .then(() => console.log("name update"))
+        .catch((error) => console.log(error));    
+        console.log(user);
+        navigate("/");
+      })
+      .catch((error) => console.log(error));
+
+   
+  };
   return (
     <div className="hero min-h-screen ">
       <div className="hero-content flex-col w-full">
@@ -16,7 +38,7 @@ const Register = () => {
               account
             </p>
           </div>
-          <div className="card-body p-0">
+          <form onSubmit={createUserHandaler} className="card-body p-0">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">User Name</span>
@@ -25,6 +47,19 @@ const Register = () => {
                 type="text"
                 placeholder="User Name"
                 className="input input-bordered"
+                name="name"
+              />
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">photoURL</span>
+              </label>
+              <input
+                type="text"
+                placeholder="photoURL"
+                className="input input-bordered"
+                name="photo"
               />
             </div>
             <div className="form-control">
@@ -32,9 +67,11 @@ const Register = () => {
                 <span className="label-text">Email</span>
               </label>
               <input
-                type="text"
+                type="email"
                 placeholder="email"
                 className="input input-bordered"
+                name="email"
+                required
               />
             </div>
             <div className="form-control">
@@ -42,9 +79,11 @@ const Register = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="text"
+                type="password"
                 placeholder="password"
                 className="input input-bordered"
+                name="password"
+                required
               />
             </div>
             <div className="form-control mt-6">
@@ -56,7 +95,7 @@ const Register = () => {
                 <button className="btn btn-active btn-link">Login</button>
               </Link>
             </p>
-          </div>
+          </form>
         </div>
       </div>
     </div>
